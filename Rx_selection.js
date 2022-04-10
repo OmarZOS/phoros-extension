283f9295b393554baec6bce8520943fb20cca22b
OmarZOS <osjfvhng16@gmail.com>
2022-04-10 11:26:03 +0100

    changed to Lamri Ali's extension

---
+++ Rx_selection.js
@@ -0,0 +1,29 @@
+ 
+
+
+function click()
+{
+       
+       
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        Tab_Rx=["facebook","twitter","linkedin"]
+
+            console.log(get_Rx(tabs[0].url));
+            if(Tab_Rx.indexOf(get_Rx(tabs[0].url)) !== -1)
+            {
+                window.location.href = get_Rx(tabs[0].url)+".html";
+                //alert(document.getElementsByClassName("fa fa-facebook"))
+                document.getElementsByClassName("fa fa-linkedin").setAttribute("style","color:red");
+            }else{
+                window.location.href ="base.html";
+            }
+           
+        });
+    
+}
+
+
+click()
+		 
+
+ 
